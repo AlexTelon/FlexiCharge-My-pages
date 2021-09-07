82aafae0d9d9ee3a4b73fdfc6fab05f95b58ff3b
khalidme93 <khalidmelkilany932@yahoo.com>
2021-09-07 16:03:17 +0200

    Firebase is initialized

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hello world
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
