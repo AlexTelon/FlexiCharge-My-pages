cc216c02b4dc04b37c60bbfc743b1ebbb31d8eba
hawkarJamal1997 <jaha1710@student.ju.se>
2021-10-01 00:22:46 +0200

    fixed error with username

--- flexicharge/src/components/AuthService.tsx
+++ flexicharge/src/components/AuthService.tsx
@@ -3,53 +3,55 @@ import axios from "axios";
 const API_URL = "http://54.220.194.65:8080/auth/";
 
 class AuthService {
-  
-    login(userName: string, password: string) {
-      console.log(userName, password)
-        return axios
-          .post(API_URL + "sign-in", {
-            username: userName,
-            password: password
-          })
-          .then(response => {
-            if (response.data.accessToken) {
-              localStorage.setItem("user", JSON.stringify(response.data));
-            }
-    
-            return response.data;
-          });
-      }
-    
-      logout() {
-        localStorage.removeItem("user");
-      }
-      
-    
-      register(UserName: string, firstName: string, familyName: string, email: string, password: string) {
-        
-        return axios.post(API_URL + "sign-up", {
-          name: firstName,
-          family_name: familyName,
-          email: email,
-          username: UserName,
-          password: password
-        });
-      }
+	login(userName: string, password: string) {
+		console.log(userName, password);
+		return axios
+			.post(API_URL + "sign-in", {
+				username: userName,
+				password: password,
+			})
+			.then((response) => {
+				if (response.data.accessToken) {
+					localStorage.setItem("user", JSON.stringify(response.data));
+				}
 
-      verify(UserName: string, code: string) {
-        
-        return axios.post(API_URL + "verify", {
-          code: code,
-          username: UserName
-        });
-      }
-    
-      getCurrentUser() {
-        const userStr = localStorage.getItem("user");
-        if (userStr) return JSON.parse(userStr);
-    
-        return null;
-      }
-    }
-    
-    export default new AuthService();
+				return response.data;
+			});
+	}
+
+	logout() {
+		localStorage.removeItem("user");
+	}
+
+	register(
+		firstName: string,
+		familyName: string,
+		email: string,
+		UserName: string,
+		password: string
+	) {
+		return axios.post(API_URL + "sign-up", {
+			name: firstName,
+			family_name: familyName,
+			email: email,
+			username: UserName,
+			password: password,
+		});
+	}
+
+	verify(UserName: string, code: string) {
+		return axios.post(API_URL + "verify", {
+			code: code,
+			username: UserName,
+		});
+	}
+
+	getCurrentUser() {
+		const userStr = localStorage.getItem("user");
+		if (userStr) return JSON.parse(userStr);
+
+		return null;
+	}
+}
+
+export default new AuthService();
--- flexicharge/src/components/validation.tsx
+++ flexicharge/src/components/validation.tsx
@@ -3,215 +3,187 @@ import AuthService from "./AuthService";
 import verusername from "../pages/VerifyAccount";
 
 const initialFormValues = {
-    username: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    code: "",
-    formSubmitted: false,
+	username: "",
+	firstName: "",
+	lastName: "",
+	email: "",
+	password: "",
+	code: "",
+	formSubmitted: false,
 };
 
-
 export const ValidationForm = () => {
-    const [values, setValues] = useState(initialFormValues);
-    const [errors, setErrors] = useState({} as any);
-    const [msg, setMsg] = useState("");
-    const [redirect, setRedirect] = useState(false);
+	const [values, setValues] = useState(initialFormValues);
+	const [errors, setErrors] = useState({} as any);
+	const [msg, setMsg] = useState("");
+	const [redirect, setRedirect] = useState(false);
 
+	const validate: any = (fieldValues = values) => {
+		// this function will check if the form values are valid
+		const temp: any = { ...errors };
 
+		if ("firstName" in fieldValues)
+			temp.firstName = fieldValues.firstName ? "" : "This field is required.";
+		if ("code" in fieldValues)
+			temp.code = fieldValues.code ? "" : "This field is required.";
 
+		if ("userName" in fieldValues)
+			temp.userName = fieldValues.username ? "" : "This field is required.";
 
+		if ("lastName" in fieldValues)
+			temp.lastName = fieldValues.lastName ? "" : "This field is required.";
 
+		if ("email" in fieldValues) {
+			temp.email = fieldValues.email ? "" : "This field is required.";
+			if (fieldValues.email)
+				temp.email = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(fieldValues.email)
+					? ""
+					: "Email is not valid.";
+		}
 
-    const validate: any = (fieldValues = values) => {
-        // this function will check if the form values are valid
-        const temp: any = { ...errors }
+		if ("password" in fieldValues) {
+			temp.password = fieldValues.password ? "" : "This field is required.";
+			if (fieldValues.password)
+				temp.password = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(
+					fieldValues.password
+				)
+					? ""
+					: "Password must meet the requirements.";
+		}
 
+		setErrors({
+			...temp,
+		});
+	};
+	const handleInputValue = (e: any) => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value,
+		});
+		validate({ [name]: value });
+	};
+	const handleInputValueLogin = (e: any) => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value,
+		});
+	};
 
+	const formIsValid = (fieldValues = values) => {
+		// this function will check if the form values and return a boolean value
+		const isValid =
+			fieldValues.firstName &&
+			fieldValues.username &&
+			fieldValues.lastName &&
+			fieldValues.email &&
+			fieldValues.password &&
+			Object.values(errors).every((x) => x === "");
+		return isValid;
+	};
+	const loginformIsValid = (fieldValues = values) => {
+		// this function will check if the form values and return a boolean value
+		const isValid =
+			fieldValues.username &&
+			fieldValues.password &&
+			Object.values(errors).every((x) => x === "");
 
-        if ("firstName" in fieldValues)
-            temp.firstName = fieldValues.firstName ? "" : "This field is required."
-        if ("code" in fieldValues)
-            temp.code = fieldValues.code ? "" : "This field is required."
+		return isValid;
+	};
 
-        if ("userName" in fieldValues)
-            temp.userName = fieldValues.username ? "" : "This field is required."
+	const verifyFormisValid = (fieldValues = values) => {
+		const isValid =
+			fieldValues.username &&
+			fieldValues.code &&
+			Object.values(errors).every((x) => x === "");
 
-        if ("lastName" in fieldValues)
-            temp.lastName = fieldValues.lastName ? "" : "This field is required."
+		return isValid;
+	};
 
-        if ("email" in fieldValues) {
-            temp.email = fieldValues.email ? "" : "This field is required."
-            if (fieldValues.email)
-                temp.email = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(fieldValues.email)
-                    ? ""
-                    : "Email is not valid."
-        }
+	const RegisterhandleFormSubmit = async (e: any) => {
+		// this function will be triggered by the submit event
+		e.preventDefault();
 
-        if ("password" in fieldValues) {
-            temp.password = fieldValues.password ? "" : "This field is required."
-            if (fieldValues.password)
-                temp.password = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(fieldValues.password)
-                    ? ""
-                    : "Password must meet the requirements."
-        }
+		const isValid = Object.values(errors).every((x) => x === "");
 
+		if (isValid) {
+			const { username, firstName, lastName, email, password } = values;
+			AuthService.register(firstName, lastName, email, username, password).then(
+				(response) => {
+					setMsg(response.data.message);
+					setRedirect(true);
+				},
+				(error) => {
+					const resMessage =
+						(error.response &&
+							error.response.data &&
+							error.response.data.message) ||
+						error.message ||
+						error.toString();
 
-        setErrors({
-            ...temp
-        });
-    }
-    const handleInputValue = (e: any) => {
-        const { name, value } = e.target;
-        setValues({
-            ...values,
-            [name]: value
-        });
-        validate({ [name]: value });
-    }
-    const handleInputValueLogin = (e: any) => {
-        const { name, value } = e.target;
-        setValues({
-            ...values,
-            [name]: value
-        });
-    }
+					setMsg(resMessage);
+				}
+			);
+		}
+	};
+	const verifyHandleFormSubmit = async (
+		e: any,
+		username: string,
+		code: any
+	) => {
+		e.preventDefault();
+		const codeVerify = code.join("");
+		AuthService.verify(username, codeVerify).then(
+			(response) => {
+				setRedirect(true);
+			},
+			(error) => {
+				const resMessage =
+					error.response && error.response.data && error.response.data.message;
+				error.message;
+				error.toString();
+				setMsg(resMessage);
+			}
+		);
+	};
 
-    const formIsValid = (fieldValues = values) => {
-        // this function will check if the form values and return a boolean value
-        const isValid =
-            fieldValues.firstName &&
-            fieldValues.username &&
-            fieldValues.lastName &&
-            fieldValues.email &&
-            fieldValues.password &&
-            Object.values(errors).every((x) => x === "");
-        return isValid;
-    }
-    const loginformIsValid = (fieldValues = values) => {
-        // this function will check if the form values and return a boolean value
-        const isValid =
+	const LogInhandleFormSubmit = async (e: any) => {
+		// this function will be triggered by the submit event
+		e.preventDefault();
 
-            fieldValues.username &&
-            fieldValues.password &&
-            Object.values(errors).every((x) => x === "");
+		const { username, password } = values;
+		AuthService.login(username, password).then(
+			(response) => {
+				console.log(username, password);
+				console.log(response, "Login", password);
+				setRedirect(true);
+			},
+			(error) => {
+				const loginMessage =
+					(error.response &&
+						error.response.data &&
+						error.response.data.message) ||
+					error.message ||
+					error.toString();
+				setMsg(loginMessage);
+				console.log(loginMessage);
+			}
+		);
+	};
 
-        return isValid;
-    }
-
-    const verifyFormisValid = (fieldValues = values) => {
-        const isValid =
-
-            fieldValues.username &&
-            fieldValues.code &&
-            Object.values(errors).every((x) => x === "");
-
-        return isValid;
-
-    }
-
-    const RegisterhandleFormSubmit = async (e: any) => {
-        // this function will be triggered by the submit event
-        e.preventDefault();
-
-        const isValid = Object.values(errors).every((x) => x === "");
-
-        if (isValid) {
-            const { username, firstName, lastName, email, password } = values
-            AuthService.register(
-                firstName,
-                lastName,
-                username,
-                email,
-                password
-            ).then(
-                response => {
-                    console.log(response, "sajdhasdhsa", firstName)
-                    setMsg(response.data.message)
-                    setRedirect(true)
-                },
-                error => {
-                    const resMessage =
-                        (error.response &&
-                            error.response.data &&
-                            error.response.data.message) ||
-                        error.message ||
-                        error.toString();
-                    console.log("i am here     ", resMessage)
-                    setMsg(resMessage)
-                }
-            );
-        }
-    }
-    const verifyHandleFormSubmit = async (
-        e: any,
-        username: string,
-        code: any
-    ) => {
-        e.preventDefault();
-        const codeVerify = code.join("");
-        AuthService.verify(username, codeVerify).then(
-            (response) => {
-                setRedirect(true);
-            },
-            (error) => {
-                const resMessage =
-                    (error.response &&
-                        error.response.data &&
-                        error.response.data.message)
-                error.message
-                error.toString();
-                setMsg(resMessage);
-            }
-        );
-    };
-
-    const LogInhandleFormSubmit = async (e: any) => {
-        // this function will be triggered by the submit event
-        e.preventDefault();
-
-
-
-        const { username, password } = values
-        AuthService.login(
-            username,
-            password
-        ).then(
-            response => {
-                console.log(username, password)
-                console.log(response, "Login", password)
-                setRedirect(true)
-
-            },
-            error => {
-                const loginMessage =
-                    (error.response &&
-                        error.response.data &&
-                        error.response.data.message) ||
-                    error.message ||
-                    error.toString();
-                setMsg(loginMessage)
-                console.log(loginMessage)
-            }
-        );
-
-    }
-
-
-
-    return {
-        values,
-        errors,
-        msg,
-        handleInputValueLogin,
-        handleInputValue,
-        loginformIsValid,
-        verifyFormisValid,
-        RegisterhandleFormSubmit,
-        LogInhandleFormSubmit,
-        formIsValid,
-        verifyHandleFormSubmit,
-        redirect
-    };
-}
+	return {
+		values,
+		errors,
+		msg,
+		handleInputValueLogin,
+		handleInputValue,
+		loginformIsValid,
+		verifyFormisValid,
+		RegisterhandleFormSubmit,
+		LogInhandleFormSubmit,
+		formIsValid,
+		verifyHandleFormSubmit,
+		redirect,
+	};
+};
