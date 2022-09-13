2737aec04626aff91dd624769c88cebefbc16a97
Sha Nawaz Hathalia <shha1695@student.ju.se>
2022-09-13 16:07:29 +0200

    Added a profile styles sheet and destroyed the user profile.

--- flexicharge/src/pages/Profile.tsx
+++ flexicharge/src/pages/Profile.tsx
@@ -1,30 +1,10 @@
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Alert } from "@material-ui/lab";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import AuthService from "../components/AuthService";
 import BottomNavigationBar from "../components/BottomNavigation";
 import Mobile from "../components/Mobile";
 import Navbar from "../components/Navbar";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    profile: {
-      spacing: 0,
-      maxWidth: "60%",
-      minWidth: "20vh",
-      minHeight: "100vh",
-      margin: "auto",
-      marginTop: "50px",
-    },
-    container: {
-      backgroundColor: theme.flexiCharge.primary.white,
-      borderRadius: "16",
-      justifyContent: "center",
-      padding: "100px",
-    },
-  })
-);
+import useStyles from "../components/styles/profileStyles.js";
 
 const Profile = () => {
   const classes = useStyles();
@@ -51,47 +31,13 @@ const Profile = () => {
       {Mobile() ? <Navbar /> : <BottomNavigationBar />}
       <div className={classes.profile}>
         <div className={classes.container}>
-          <div className="card mb-3">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-sm-3">
-                  <h6 className="mb-0">First Name</h6>
-                </div>
-                <div className="col-sm-9 text-secondary">{firstName}</div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <h6 className="mb-0">Family Name</h6>
-                </div>
-                <div className="col-sm-9 text-secondary">{familyName}</div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <h6 className="mb-0">Username</h6>
-                </div>
-                <div className="col-sm-9 text-secondary">{userName}</div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-3">
-                  <h6 className="mb-0">Email</h6>
-                </div>
-                <div className="col-sm-9 text-secondary">{email}</div>
-              </div>
-              <hr />
-              <button
-                type="button"
-                className="btn btn-success"
-                onClick={(e) => {
-                  e.preventDefault();
-                  history.push("/change-password");
-                }}
-              >
-                Change Password
-              </button>
-            </div>
+          <div className={classes.profile__items}>
+            <div className={classes.profileInfo}>Firstname</div>
+            <div>Ezaz</div>
+          </div>
+          <div className={classes.profile__items}>
+            <div className={classes.profileInfo}>Lastname</div>
+            <div>Hathalia</div>
           </div>
         </div>
       </div>
