30b12337d2c4fb2b869ffda32bf19ad504a54163
Sha Nawaz Hathalia <shha1695@student.ju.se>
2022-09-13 13:54:11 +0200

    added a LoginStyle sheet for the Css component

--- flexicharge/src/pages/Login.tsx
+++ flexicharge/src/pages/Login.tsx
@@ -7,60 +7,7 @@ import LockIcon from "@material-ui/icons/Lock";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import { ValidationForm } from "../components/validation";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    grid: {
-      alignItems: "center",
-      justifyContent: "center",
-      maxWidth: "40%",
-      minWidth: "40vh",
-      minHeight: "100vh",
-      height: "0",
-      margin: "auto",
-    },
-    container: {
-      backgroundColor: theme.flexiCharge.primary.white,
-      borderRadius: "0.5rem",
-      justifyContent: "center",
-      paddingTop: "36px",
-      paddingBottom: "24px",
-    },
-    gridItem: {
-      margin: "auto",
-    },
-    textFields: {
-      maxWidth: "80%",
-      marginTop: "24px",
-    },
-    button: {
-      "&:hover": {
-        transform: "translateY(2px)",
-        boxShadow: theme.flexiCharge.boxShadow.button,
-        backgroundColor: theme.palette.primary.dark,
-        color: theme.flexiCharge.primary.white,
-      },
-      marginTop: "48px",
-      marginBottom: "32px",
-      width: "50%",
-      minHeight: "3rem",
-      backgroundColor: theme.flexiCharge.accent.primary,
-    },
-    links: {
-      marginTop: theme.spacing(2),
-      fontWeight: 500,
-    },
-    backdrop: {
-      position: "absolute" as "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      width: 400,
-      bgcolor: "background.paper",
-      p: 4,
-    },
-  })
-);
+import useStyles from "../components/LoginStyles.js";
 
 const inputFieldValues = [
   {
