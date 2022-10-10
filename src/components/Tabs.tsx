c15ed57988fed2fe97d2414c634f494f102a1229
sebastian <zese1798@student.ju.se>
2022-10-10 02:48:04 +0200

    implemented the invoices tab, our spa is not mobile first not mobile friendly at all atm

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -9,7 +9,7 @@ import { useHistory } from "react-router";
 import { useEffect, useState } from "react";
 import AuthService from "../components/AuthService";
 import { PropaneSharp } from "@mui/icons-material";
-import InvoicesList from "./invoicesList/InvoicesList";
+import InvoicesTab from "./invoicesTab/InvoicesTab";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -54,6 +54,7 @@ export default function BasicTabs() {
   const [phone, setPhoneNumber] = useState("");
   const [address, setAddress] = useState("");
   const [userName, setUserName] = useState("");
+  const [userId, setuserId] = useState("");
   const history = useHistory();
 
   useEffect(() => {
@@ -67,6 +68,7 @@ export default function BasicTabs() {
       setPhoneNumber(currentUser.phone);
       setAddress(currentUser.address);
       setUserName(currentUser.username);
+      setuserId(currentUser.user_id);
     }
   }, []);
 
@@ -92,7 +94,7 @@ export default function BasicTabs() {
     color: "#333 !important",
   };
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: "100%"}}>
       <Box sx={{ borderBottom: 10, borderColor: "#e5e5e5" }}>
         <Tabs
           variant="fullWidth"
@@ -143,7 +145,7 @@ export default function BasicTabs() {
         Here will the users charging history be
       </TabPanel>
       <TabPanel value={value} index={2}>
-        <InvoicesList/>
+        <InvoicesTab UserId = {userId}/>
       </TabPanel>
     </Box>
   );
