8fa931f47e84edc3d528296385d25d90a5dc0710
Gabriel Adward <adga21bv@student.ju.se>
2023-09-19 14:06:05 +0200

    The bug is solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
-//hello
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
+import Theme from "./theme/theme";
+import AuthService from "./components/AuthService";
 import Login from "./pages/Login";
-import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import ChargingSessions from "./pages/ChargingSessions";
 import VerifyAccount from "./pages/VerifyAccount";
-import { ThemeProvider } from "@material-ui/core";
-import Theme from "./theme/theme";
 import ForgotPassword from "./pages/ForgotPassword";
 import ConfirmForgotPassword from "./pages/ConfirmForgotPassword";
-import AuthService from "./components/AuthService";
 import Profile from "./pages/Profile";
 import ChangePassword from "./pages/ChangePassword";
 
@@ -25,13 +25,13 @@ function App() {
     <ThemeProvider theme={Theme}>
       <div className="App">
         <BrowserRouter>
-          <main>
+          <Switch>
             <Route exact path="/">
               <Redirect to={redirectPath} />
             </Route>
 
             <Route path="/sign-in">
-              {isLoggedIn ? <Redirect to="/profile" /> : () => <Login />}
+              {isLoggedIn ? <Redirect to="/profile" /> : <Login />}
             </Route>
 
             <Route path="/profile">
@@ -40,17 +40,19 @@ function App() {
 
             <Route path="/sign-up" component={Register} />
             <Route path="/verify" component={VerifyAccount} />
-            <Route
-              path="/forgot-password"
-              component={() => <ForgotPassword />}
-            />
+            <Route path="/forgot-password" component={ForgotPassword} />
             <Route
               path="/confirm-forgot-password"
               component={ConfirmForgotPassword}
             />
             <Route path="/change-password" component={ChangePassword} />
             <Route path="/charging-sessions" component={ChargingSessions} />
-          </main>
+
+            {/* Add a catch-all route for 404 */}
+            <Route path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
         </BrowserRouter>
       </div>
     </ThemeProvider>
