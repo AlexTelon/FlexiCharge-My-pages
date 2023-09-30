9748ab4607295577513d7ac1b77fa0b9f8b2ccf7
flrn579 <143874663+flrn579@users.noreply.github.com>
2023-09-30 14:20:02 +0200

    Fixed an issue that the sign out button won't be pushed out of the screen #157

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -146,7 +146,7 @@ export default function BasicTabs() {
   return (
     <Box sx={{ width: "100%", height: "100%", display: "grid", overflow: "auto", gridTemplateColumns: "20% 80%" }}>
       <Box>
-        <Box sx={{ height: "100%", position: "sticky", top: "0", left: "0", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+        <Box sx={{ height: "100vh", position: "sticky", top: "0", left: "0", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
           <img className={classes.navLogo} src={FlexiChargeLogo} />
 
           <Tabs
